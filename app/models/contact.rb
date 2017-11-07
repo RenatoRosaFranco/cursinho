b47940fe3ca938deaca3e5b38ef7d389334deb84
Renato Franco <renato_ny@live.com>
2017-11-07 14:01:58 -0200

    Contact model

---
+++ app/models/contact.rb
@@ -0,0 +1,52 @@
+class Contact 
+
+	include ActiveModel::Conversion
+ 	include ActiveModel::Validations
+
+	attr_accessor :name, :phone, :email, :message
+
+	def initialize(params = {})
+		@name = params[:name]
+		@phone = params[:phone]
+		@email = params[:email]
+		@message = params[:message]
+	end
+
+	def self.sendEmail
+	  begin
+		ContactMailer.sended(self).deliver_now
+		ContactMailer.received(self).deliver_now
+	  rescue Exception => e 
+	  	raise StandardError, e
+	  end
+	end
+
+	def persisted?
+		return false
+	end
+
+	validates :name,
+	          presence: true,
+	          uniqueness: false,
+	          allow_blank: false,
+	          length: {minimum: 3, maximum: 30}
+
+	validates :phone,
+			  presence: true,
+			  uniqueness: false,
+			  allow_blank: false,
+			  length: {minimum: 5, maximum: 15}
+
+	validates :email,
+	          presence: true,
+	          uniqueness: false,
+	          allow_blank: false,
+	          length: {minimum: 3, maximum: 245}
+
+	validates :message,	
+			  presence: true,
+			  uniqueness: false,
+			  allow_blank: false,
+			  length: {minimum: 15, maximum: 800}
+
+end
